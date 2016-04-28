8455705a7a3d4a065297aa29b2c8b2ce617928e3
binli <binli@deepglint.com>
2016-04-28 12:24:13 +0800

    only for kitti

---
+++ train.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+GPU_ID=0
+NET=VGG16
+#VGG16 GoogLeNet
+NET_lc=${NET,,}
+DATASET=kitti
+PRETRAINWEIGHT="./data/imagenet_models/VGG16.v2.caffemodel"
+
+case $DATASET in
+  kitti)
+    TRAIN_IMDB="kitti_train"
+    TEST_IMDB="kitti_test"
+    PT_DIR="kitti"
+    ITERS=100000
+    ;;
+  *)
+    echo "No dataset given"
+    exit
+    ;;
+esac
+
+LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time python ./tools/train_net.py --gpu ${GPU_ID} \
+  --solver models/${PT_DIR}/${NET}/solver.prototxt \
+  --weights ${PRETRAINWEIGHT} \
+  --imdb ${TRAIN_IMDB} \
+  --iters ${ITERS} \
+  --cfg experiments/cfgs/faster_rcnn_end2end.yml
